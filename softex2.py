60e82f65c5932697fd0bf6ef112412d9964f39a2
Sérgio Ricardo <serginhofilho.16@gmail.com>
2022-07-24 22:26:15 -0300

    git commit

--- softex2.py
+++ softex2.py
@@ -6,19 +6,21 @@ while c == "c":
     subtracao= num_1 - num_2
     multiplicacao= num_1 * num_2
     divisao= num_1 / num_2
-    operacao = str(input("Qual operação voce deseja \n+ para soma\n- para subtração\n* para multiplicação\n/ para divisão\n"))
-    if operacao == "+":
+    operacao = str(input("Qual operação voce deseja \n1 para soma\n2 para subtração\n3 para multiplicação\n4 para divisão\n0 para sair\n"))
+    if operacao == "1":
         print(soma)
         c = str(input("Digite c para continuar: "))
-    elif operacao == "-":
+    elif operacao == "2":
         print(subtracao)
         c = str(input("Digite c para continuar: "))
-    elif operacao == "*":
+    elif operacao == "3":
         print(multiplicacao)
         c = str(input("Digite c para continuar: "))
-    elif operacao == "/":
+    elif operacao == "4":
         print(divisao)
         c = str(input("Digite c para continuar: "))
+    elif operacao == "0":
+        break
     else:
         print(0)
         
