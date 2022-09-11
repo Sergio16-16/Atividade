4cab139ee62dae08698c99991bf0b7ea1a9f6548
Sérgio Ricardo <84404213+Sergio16-16@users.noreply.github.com>
2022-09-11 19:12:31 -0300

    Update Manipulação_String.py

--- Manipulação_String.py
+++ Manipulação_String.py
@@ -3,9 +3,9 @@ Codigo_1 = input("Digite uma frase ou palavra: ")
 len(Codigo_1)
 print(len(Codigo_1))
 
-##String para letra maiuscula
+##String para letra maiúscula
 Codigo_2 = input("Digite uma frase ou palavra: ").upper()
 print(Codigo_2)
-##
+##String para letra miniscula
 Codigo_3 = input("Digite uma frase ou palavra: ").lower()
 print(Codigo_3)
