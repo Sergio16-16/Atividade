05e99eed8cccd62bfca7758ce1e362463b197565
Sérgio Ricardo <84404213+Sergio16-16@users.noreply.github.com>
2022-09-11 19:11:11 -0300

    Add files via upload

---
+++ Manipulação_String.py
@@ -0,0 +1,11 @@
+##Tamanho da String
+Codigo_1 = input("Digite uma frase ou palavra: ")
+len(Codigo_1)
+print(len(Codigo_1))
+
+##String para letra maiuscula
+Codigo_2 = input("Digite uma frase ou palavra: ").upper()
+print(Codigo_2)
+##
+Codigo_3 = input("Digite uma frase ou palavra: ").lower()
+print(Codigo_3)
